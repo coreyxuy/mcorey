faa8a8acfcca2bbd3b23a97c81ffd109030c6ff7
corey <1595162370@qq.com>
2018-12-20 16:13:25 +0800

    项目初始化提交代码

---
+++ src/main/java/com/itcorey/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.itcorey.dao;
+
+import com.itcorey.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
--- src/main/java/vo/user.java
+++
@@ -1,8 +0,0 @@
-package vo;
-
-/**
- * Created by ：Corey
- * 11:41 2018/12/19
- */
-public class user {
-}
